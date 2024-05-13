ba88fbabedfab031dcf19326b5fbf32c212214c3
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-05-13 16:03:28 +0200

    services image optimization

--- components/ServicesSection.jsx
+++ components/ServicesSection.jsx
@@ -2,12 +2,16 @@ import styles from './ServicesSection.module.scss';
 import Image from 'next/image';
 import Accordions from './Accordions';
 import servicesImage from '@/public/beehive-services.png';
+import servicesImageMobile from '@/public/beehive-services-mobile.png';
 
 const ServicesSection = () => {
   return (
     <section className={styles.servicesSection}>
       <picture>
-        <source srcSet="/beehive-services-mobile.png" media='(max-width: 1024px)' />
+        <source
+          srcSet={servicesImageMobile.src}
+          media='(max-width: 1024px)'
+        />
         <Image src={servicesImage} alt='Beehive Services' />
       </picture>
       <div className={styles.servicesInfoWrapper}>
